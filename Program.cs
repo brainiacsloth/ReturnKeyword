7e0b4b024a674cfca1ea741649b000617699d722
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-06 09:18:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a value.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.Write("the cube of the number you entered is ");
+            Console.WriteLine(Cube(userInput));
+
+            Console.ReadLine(); 
+
+        }
+
+        static int Cube(int x)
+        {
+          return x * x * x;
+        }
+    }
+}
